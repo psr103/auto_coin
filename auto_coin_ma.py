bab2b3f7f2a8657bc6f6352bc5063b083e00a8c6
Ryool <43835261+psr103@users.noreply.github.com>
2022-02-23 15:32:44 +0900

    first

--- auto_coin_ma.py
+++ auto_coin_ma.py
@@ -14,25 +14,21 @@ def post_message(token, channel, text):
     )
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_ma15(ticker):
-    """15일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
     ma15 = df['close'].rolling(15).mean().iloc[-1]
     return ma15
 
 def get_balance(ticker):
-    """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -43,14 +39,13 @@ def get_balance(ticker):
     return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
-# 로그인
+
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-# 자동매매 시작
+
 while True:
     try:
         now = datetime.datetime.now()
@@ -67,9 +62,9 @@ while True:
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
                     krw = get_balance("KRW")
                     btc = get_balance("BTC")
-                    post_message(myToken,"#autocoin","매수 진행")
-                    post_message(myToken,"#autocoin","원화 잔액: %s")%krw
-                    post_message(myToken,"#autocoin","코인 잔액: %s")%btc
+                    post_message(myToken,"#autocoin","Buying in progress")
+                    post_message(myToken,"#autocoin","KRW: %s")%krw
+                    post_message(myToken,"#autocoin","BTC: %s")%btc
                     
                     
         else:
@@ -78,9 +73,9 @@ while True:
                 upbit.sell_market_order("KRW-BTC", btc*0.9995)
                 krw = get_balance("KRW")
                 btc = get_balance("BTC")
-                post_message(myToken,"#autocoin","전량 매도 진행")
-                post_message(myToken,"#autocoin","원화 잔액: %s")%krw
-                post_message(myToken,"#autocoin","코인 잔액: %s")%btc
+                post_message(myToken,"#autocoin","Solding in progress")
+                post_message(myToken,"#autocoin","KRW: %s")%krw
+                post_message(myToken,"#autocoin","BTC: %s")%btc
         time.sleep(1)
     except Exception as e:
         print(e)
